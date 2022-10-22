ada52106f25e18f9b935ae4d751012b0711d3de4
Xudoberdi G'ayratov <115026746+Gayratoff@users.noreply.github.com>
2022-10-22 21:34:50 +0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,8 +1,18 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
+from utils.db_api.SQLDB import Database
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+base =Database(path_to_db='baza.db')
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from utils.db_api.SQLDB import Database
+from data import config
+
+bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+base =Database(path_to_db='baza.db')
